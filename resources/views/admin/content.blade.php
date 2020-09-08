d5cb9b704d6280d0958a351e69072259f3a53a17
Kevin <863129201@qq.com>
2020-09-08 10:50:03 +0800

    重写面包屑，改为语言文件配置并翻译

--- resources/views/admin/content.blade.php
+++ resources/views/admin/content.blade.php
@@ -40,10 +40,10 @@
         </ol>
         @elseif(config('admin.enable_default_breadcrumb'))
         <ol class="breadcrumb" style="margin-right: 30px;">
-            <li><a href="{{ admin_url('/') }}"><i class="fa fa-dashboard"></i> {{__('Home')}}</a></li>
+            <li><a href="{{ admin_url('/') }}"><i class="fa fa-dashboard"></i> {{trans('admin.breadcrumb.home') }}</a></li>
             @for($i = 2; $i <= count(Request::segments()); $i++)
                 <li>
-                {{ucfirst(Request::segment($i))}}
+                {{ trans('admin.breadcrumb.'.Request::segment($i)) }}
                 </li>
             @endfor
         </ol>
